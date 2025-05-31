75280822516cfed975654985295e6cd4ae64d6a8
Cheryl Lee <clee0937@gmail.com>
2025-05-31 20:46:59 +0100

    completed validate_pin.py

---
+++ challenges/python/validate_pin.py
@@ -0,0 +1,20 @@
+ #CodeWars Challenge: [Validate PIN]
+ #Link: [https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/train/python]
+ #Difficulty: [7 Kyu]
+
+# Instructions
+
+# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. If the function is passed a valid PIN string, return true, else return false.
+
+# Examples
+# "1234"   -->  true
+# "12345"  -->  false
+# "a234"   -->  false
+
+# 1
+def validate_pin(pin):
+    return pin.isdigit() and len(pin) in (4, 6)
+
+# 2
+def validate_pin(pin):
+    return pin.isdigit() and (len(pin) == 6 or len(pin) == 4)
