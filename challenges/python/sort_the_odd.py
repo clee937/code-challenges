01e57b32466df23b97a0cabbb43324932c473e19
Cheryl Lee <clee0937@gmail.com>
2025-09-29 23:08:55 +0100

    completed sort_the_odd.py

--- challenges/python/sort_the_odd.py
+++ challenges/python/sort_the_odd.py
@@ -41,6 +41,14 @@ def sort_odd_numbers_in_array(source_array):
     
     return [next(odd_iter) if num % 2 != 0 else num for num in source_array]
 
+# 3
+def sort_odd_numbers_in_array(source_array):
+    odds = sorted((x for x in source_array if x%2), reverse=True)
+    # Without reverse, you can use .pop(0), but that is linear time operation, as opposed to just pop() which is constant time. pop(0) and reverse take about the same amount of time, but with reverse you only need to do it once, instead of .pop(0) for (potentially) every single element.
+    
+    return [x if x%2==0 else odds.pop() for x in source_array]
+
+
 print(sort_odd_numbers_in_array([7, 1])) # --> [1, 7]
 print(sort_odd_numbers_in_array([5, 8, 6, 3, 4])) # --> [3, 8, 6, 5, 4]
 print(sort_odd_numbers_in_array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])) # --> [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
