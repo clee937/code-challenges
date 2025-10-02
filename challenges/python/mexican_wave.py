01586ef40b7c423a5a69cde518e1ed69817ce34c
Cheryl Lee <clee0937@gmail.com>
2025-10-02 23:11:33 +0100

    added mexican_wave.py

---
+++ challenges/python/mexican_wave.py
@@ -0,0 +1,13 @@
+# CodeWars Challenge: [Mexican Wave]
+# Link: [https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/python]
+# Difficulty: [6 Kyu]
+
+# Instructions
+# In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return an array of strings where an uppercase letter is a person standing up. The input string will always consist of lowercase letters and spaces, but may be empty, in which case you must return an empty array. If the character in the string is whitespace then pass over it as if it was an empty seat
+
+# Examples
+# "hello" => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+# " s p a c e s " => [ " S p a c e s ", " s P a c e s ", " s p A c e s ", " s p a C e s ", " s p a c E s ", " s p a c e S "]
+
+def wave(string):
+    pass
