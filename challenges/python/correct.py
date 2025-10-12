3bbb495c8966bc926d74460d1d6eb4e21728948b
Cheryl Lee <clee0937@gmail.com>
2025-10-12 21:24:23 +0100

    completed correct.py

---
+++ challenges/python/correct.py
@@ -0,0 +1,20 @@
+# CodeWars Challenge: [Character Recognition Errors]
+# Link: [https://www.codewars.com/kata/577bd026df78c19bca0002c0/train/python]
+# Difficulty: [8 Kyu]
+
+# Instructions
+# Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer. When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes. Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+# S is misinterpreted as 5
+# O is misinterpreted as 0
+# I is misinterpreted as 1
+# The test cases contain numbers only by mistake.
+
+def correct(string):
+    return string.translate(str.maketrans("501", "SOI"))
+
+# print(correct("L0ND0N"))
+# print(correct("DUBL1N"))
+# print(correct("51NGAP0RE"))
+# print(correct("BUDAPE5T"))
+# print(correct("PAR15"))
