2d80d659fdb76423906b5f63d546b04d19c3c4b4
Cheryl Lee <clee0937@gmail.com>
2025-10-24 17:56:31 +0100

    completed extract_domain_name.py

--- challenges/python/extract_domain_name.py
+++ challenges/python/extract_domain_name.py
@@ -9,8 +9,22 @@
 # * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 # * url = "https://www.cnet.com"                -> domain name = cnet"
 
+# with regex
+import re
+
+
 def domain_name(url):
-    pass
+    return re.search(r'(https?://)?(www\.)?([^/.]+)', url).group(3)
+
+
+# Using string methods
+def domain_name(url):
+    url = url.replace("https://", "").replace("http://", "")
+    # if statement ensures www is only removed/replaced if at the beginning, and not if it's in the middle or the end.
+    if url[0:4] == 'www.':
+        url = url[4:]
+        # url = url.replace("www.", "")
+    return url.split(".")[0]
 
 
 print(domain_name("http://google.com"))  # --> "google"
@@ -18,3 +32,21 @@ print(domain_name("http://google.co.jp"))  # --> "google"
 print(domain_name("https://123.net"))  # --> "123"
 print(domain_name("https://hyphen-site.org"))  # --> "hyphen-site"
 print(domain_name("www.xakep.ru"))  # --> "xakep"
+print(domain_name("http://www.codewars.com/kata/"))
+print(domain_name("icann.org"))  # --> "icann"
+print(domain_name("ssec1qe2.jp/index.php"))  # --> "ssec1qe2"
+print(domain_name("vpfqtmvu.co.za/default.html"))  # --> "vpfqtmvu"
+print(domain_name("etesvx67h3tcia0v3c3gsf3whjs.tv/default.html"))
+# --> "etesvx67h3tcia0v3c3gsf3whjs"
+print(domain_name("2vbah3c0e6h86g.br/default.html"))
+# --> "2vbah3c0e6h86g"
+print(domain_name("http://5ijyb36sb7c.jp/index.php"))
+# --> "5ijyb36sb7c"
+print(domain_name("ssec1qe2.jp/index.php"))
+# --> "ssec1qe2"
+print(domain_name("zu2jz61nkddo5vn7pi46gw4xnj6so.edu/index.php"))
+# --> "zu2jz61nkddo5vn7pi46gw4xnj6so"
+print(domain_name('focksvn4w2d6deni0fhsmb.us/default.html'))
+# --> "focksvn4w2d6deni0fhsmb"
+print(domain_name("googlewww.com"))
+# --> "googlewww"
