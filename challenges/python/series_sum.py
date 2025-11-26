f700de23b2be3ed9429ea83678b51d7c4b9e60e9
Cheryl Lee <clee0937@gmail.com>
2025-11-26 12:41:07 +0000

    completed series_sum.py

--- challenges/python/series_sum.py
+++ challenges/python/series_sum.py
@@ -12,15 +12,31 @@
 
 # Rules
 # You need to round the answer to 2 decimal places and return it as a string.
-# If the given value is 0 then it should return "0.00".
+# If the given value is 0 then it should return '0.00'.
 # You will only be given Natural Numbers as arguments.
 
 # Examples (Input --> Output)
 
 # n
-# 1 --> 1 --> "1.00"
-# 2 --> 1 + 1/4 --> "1.25"
-# 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+# 1 --> 1 --> '1.00'
+# 2 --> 1 + 1/4 --> '1.25'
+# 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> '1.57'
 
 def series_sum(n):
-    pass
+    return f'{sum(1/d for d in range(1, n*3, 3)):.2f}'
+
+
+def series_sum(n):
+    result = 0
+
+    for d in range(1, n*3, 3):
+        result += 1/d
+
+    return f'{result:.2f}'
+
+
+print(series_sum(0))  # --> '0.00'
+print(series_sum(1))  # --> '1.00'
+print(series_sum(2))  # --> '1.25'
+print(series_sum(3))  # --> '1.39'
+print(series_sum(5))  # --> '1.57'
