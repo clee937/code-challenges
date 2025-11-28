d8d83c29c4a9f3c0f75d093f90018c6a465a5dbe
Cheryl Lee <clee0937@gmail.com>
2025-11-28 23:53:26 +0000

    added powers_of_2.py

---
+++ challenges/python/powers_of_2.py
@@ -0,0 +1,15 @@
+# CodeWars Challenge: [Powers of 2]
+# Link: [https://www.codewars.com/kata/57a083a57cb1f31db7000028/python]
+# Difficulty: [8 Kyu]
+
+# Instructions
+# Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+# Examples
+
+# n = 0  ==> [1]        # [2^0]
+# n = 1  ==> [1, 2]     # [2^0, 2^1]
+# n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+def powers_of_two(n):
+    pass
