549c1e4081cc451ce6a0f7ca3a47ada46576e04d
Cheryl Lee <clee0937@gmail.com>
2025-11-30 14:20:11 +0000

    completed powers_of_2.py

--- challenges/python/powers_of_2.py
+++ challenges/python/powers_of_2.py
@@ -12,4 +12,19 @@
 # n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 
 def powers_of_two(n):
-    pass
+    return [2 ** i for i in range(n+1)]
+
+
+def powers_of_two(n):
+    result = []
+
+    for i in range(n+1):
+        result.append(2**i)
+
+    return result
+
+
+print(powers_of_two(0))  # --> [1]
+print(powers_of_two(1))  # --> [1, 2]
+print(powers_of_two(2))  # --> [1, 2, 4]
+print(powers_of_two(4))  # --> [1, 2, 4, 8, 16]
