1a99f3cd37237ca58ab48dbf4f8fa9300ebd3df3
Cheryl Lee <clee0937@gmail.com>
2026-02-14 16:55:06 +0000

    completed how_much_i_love_you.py

--- challenges/python/how_much_i_love_you.py
+++ challenges/python/how_much_i_love_you.py
@@ -16,23 +16,23 @@
 
 # list
 def how_much_i_love_you(nb_petals):
-    num_of_petals = ["I love you", "a little",
-                     "a lot", "passionately", "madly", "not at all"]
+    phrases = ["I love you", "a little",
+               "a lot", "passionately", "madly", "not at all"]
 
-    return num_of_petals[(nb_petals-1) % len(num_of_petals)]
+    return phrases[(nb_petals-1) % len(phrases)]
 
 
 # dictionary
 def how_much_i_love_you(nb_petals):
 
-    num_of_petals = {1: "I love you",
-                     2: "a little",
-                     3: "a lot",
-                     4: "passionately",
-                     5: "madly",
-                     0: "not at all"}
+    phrases = {1: "I love you",
+               2: "a little",
+               3: "a lot",
+               4: "passionately",
+               5: "madly",
+               0: "not at all"}
 
-    return num_of_petals[nb_petals % 6]
+    return phrases[nb_petals % 6]
 
 
 def how_much_i_love_you(nb_petals):
