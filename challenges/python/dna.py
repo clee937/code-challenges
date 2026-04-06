dd255b746449f1b3bced9c6e980c5813f96a9bd7
Cheryl Lee <clee0937@gmail.com>
2026-04-06 15:19:48 +0100

    added dna.py

---
+++ challenges/python/dna.py
@@ -0,0 +1,21 @@
+# In this DNA sequence, which is the letter that first occurs 4 times in a
+# row?
+#
+#AAACTTTGGGCCAACCGGGAGGGTTTGCATTTAAATTTGGACCGCCAAACCTGGAAAGGGCCGATTTGGCCCTTAGGTAAATTGCCGGTTTCCCTCCTTTAATTCCGGTGGCCTTGGGGATTCAACCCAAGGACCCAAGGGATTTGCCAATTTCCAGGAAACCCAAACCCAAAGGAAATTTAAATTTCATTTCCCTTTAAACTTAAAATTGTAA
+
+dna = "AAACTTTGGGCCAACCGGGAGGGTTTGCATTTAAATTTGGACCGCCAAACCTGGAAAGGGCCGATTTGGCCCTTAGGTAAATTGCCGGTTTCCCTCCTTTAATTCCGGTGGCCTTGGGGATTCAACCCAAGGACCCAAGGGATTTGCCAATTTCCAGGAAACCCAAACCCAAAGGAAATTTAAATTTCATTTCCCTTTAAACTTAAAATTGTAA"
+
+letter = ''
+
+for char in dna:
+    if letter == '':
+        letter += char
+    elif char == letter[0]:
+        letter += char
+        if len(letter) == 4:
+            print(
+                f'{letter[0]} is the first letter that occurs 4 times in a row')
+            break
+    elif char != letter[0]:
+        letter = ''
+        letter += char
